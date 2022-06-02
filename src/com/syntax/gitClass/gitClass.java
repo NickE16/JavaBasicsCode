eade3f424d54b1c0d78800c6a7832dd8367df7fe
NickE16 <nre45854@gmail.com>
2022-06-01 21:31:26 -0400

    modified the git gitClass

--- src/com/syntax/gitClass/gitClass.java
+++ src/com/syntax/gitClass/gitClass.java
@@ -5,6 +5,7 @@ public class gitClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("I love Tati");
+		System.out.println("Why You");
 		
 	}
 
