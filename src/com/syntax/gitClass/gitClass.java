7b4f4f6c2ddd6e8ddacaff825d95ff4af8f40305
NickE16 <nre45854@gmail.com>
2022-06-01 21:21:03 -0400

    added a git gitClass

---
+++ src/com/syntax/gitClass/gitClass.java
@@ -0,0 +1,11 @@
+package com.syntax.gitClass;
+
+public class gitClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I love Tati");
+		
+	}
+
+}
